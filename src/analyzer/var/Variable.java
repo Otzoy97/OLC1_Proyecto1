a1a67ccc185699d8f757b0fd4ea95ea813cb128f
otzoy <otzoy@DESKTOP-J91V7QA>
2019-03-23 18:27:34 -0600

    - Se cambió la estructura de la variable

--- src/analyzer/var/Variable.java
+++ src/analyzer/var/Variable.java
@@ -11,10 +11,7 @@ package analyzer.var;
  * @author otzoy
  */
 public class Variable {
-    private int entero;
-    private double decimal;
-    private boolean boleano;
-    private String cadena;
+    private Object objeto;
     private Var tipo;
     private String identificador;
     private int fila, columna;
@@ -22,93 +19,33 @@ public class Variable {
     public enum Var {
         ENTERO, DECIMAL, BOOLEANO, CADENA;
     }
-    
-    /**
-     * Constructor para una variable de tipo entero
-     * @param entero
-     * @param identificador
-     * @param fila
-     * @param columna 
-     */
-    public Variable(int entero, String identificador, int fila, int columna) {
-        this.entero = entero;
-        this.tipo = Var.ENTERO;
+
+    public Variable(Object objeto, Var tipo, String identificador, int fila, int columna) {
+        this.objeto = objeto;
+        this.tipo = tipo;
         this.identificador = identificador;
         this.fila = fila;
         this.columna = columna;
     }
-    /**
-     * Constructor para una variable de tipo double
-     * @param decimal
-     * @param identificador
-     * @param fila
-     * @param columna 
-     */
-    public Variable(double decimal, String identificador,  int fila, int columna) {
-        this.decimal = decimal;
-        this.tipo = Var.DECIMAL;
+
+    public void setObjeto(Object objeto) {
+        this.objeto = objeto;
+    }
+
+    public void setTipo(Var tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setIdentificador(String identificador) {
         this.identificador = identificador;
+    }
+
+    public void setFila(int fila) {
         this.fila = fila;
+    }
+
+    public void setColumna(int columna) {
         this.columna = columna;
     }
-    /**
-     * Constructor para una variable booleana
-     * @param boleano
-     * @param identificador
-     * @param fila
-     * @param columna 
-     */
-    public Variable(boolean boleano, String identificador,  int fila, int columna) {
-        this.boleano = boleano;
-        this.tipo = Var.BOOLEANO;
-        this.identificador = identificador;
-        this.fila = fila;
-        this.columna = columna;
-    }
-    /**
-     * Constructor para una variable string
-     * @param cadena
-     * @param identificador
-     * @param fila
-     * @param columna 
-     */
-    public Variable(String cadena, String identificador,  int fila, int columna) {
-        this.cadena = cadena;
-        this.tipo = Var.CADENA;
-        this.identificador = identificador;
-        this.fila = fila;
-        this.columna = columna;
-    }
-    
-    public int getEntero() {
-        return entero;
-    }
-
-    public double getDecimal() {
-        return decimal;
-    }
-
-    public int getFila() {
-        return fila;
-    }
-
-    public int getColumna() {
-        return columna;
-    }
-
-    public boolean getBoleano() {
-        return boleano;
-    }
-
-    public String getCadena() {
-        return cadena;
-    }
-
-    public Var getTipo() {
-        return tipo;
-    }
-
-    public String getIdentificador() {
-        return identificador;
-    }
+    9
 }
