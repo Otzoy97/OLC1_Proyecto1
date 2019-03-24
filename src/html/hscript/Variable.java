a662aaf3b84fff80a7d2898abe3cc83ce05781ea
otzoy <otzoy@DESKTOP-J91V7QA>
2019-03-24 17:39:53 -0600

    - Se modificó la forma de salida al informe del Variables

--- src/html/hscript/Variable.java
+++ src/html/hscript/Variable.java
@@ -38,5 +38,9 @@ public class Variable implements HScript {
         return columna;
     }
 
+    public Object[] getFila(int contador){
+        Object[] objeto = new Object[]{this.identificador, this.objeto.getClass().getSimpleName(), this.objeto, this.fila, this.columna};
+        return objeto;
+    }
 
 }
